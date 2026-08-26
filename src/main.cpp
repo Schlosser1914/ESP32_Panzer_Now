14da16f9998a5da304056c35c0b8855531ec31f8
Schlosser1914 <schlosser.s@proton.me>
2026-08-26 16:22:06 +0200

    configuring the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
 #include <Arduino.h>
+#include <esp_now.h>
+
+#include "steppermotor.h"
 #include "motor.h"
 
 
+// Datenstruktur passend zum Sender-Repository
+typedef struct struct_message {
+    int poti_left;
+    int poti_right;
+} struct_message;
+
+struct_message empfangeneDaten;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Initialisierung der Motor-Objekte mit den entsprechenden GPIO-Pins
+motor motorLinks(25, 26, 27);
+motor motorRechts(21, 19, 18);
 
 void setup() {
 printf("works on my machine");
